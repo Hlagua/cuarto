169418dfcbe473cdbfaf5e4b665a5c600ccfe04b
Hlagua <hlagua6116@uta.edu.ec>
2026-05-28 23:59:27 -0500

    Update .gitignore to exclude generated PDFs and refactor database connection method in conexion.php; add new report PDF and update Jasper report file

--- models/conexion.php
+++ models/conexion.php
@@ -3,7 +3,8 @@ $servername = "localhost:3306";
 $dbname = "cuarto";
 $username = "root";
 $password = "";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+//$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn=new mysqli($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Conexión fallida: " . mysqli_connect_error());
 }
