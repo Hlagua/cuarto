54b97f8ad3cfc7ce0b13f0c0328f9c2a6446e7bf
Hlagua <hlagua6116@uta.edu.ec>
2026-06-05 20:06:29 -0500

    conexion para railway

--- models/conexion.php
+++ models/conexion.php
@@ -10,7 +10,6 @@ $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-echo "¡Conectado exitosamente a la nube!";
 
 
 ?>
