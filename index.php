9277c2634c7587639775f312b483837fbc06ef27
Hlagua <hlagua6116@uta.edu.ec>
2026-06-08 21:09:06 -0500

    arreglo permisos

--- index.php
+++ index.php
@@ -8,15 +8,6 @@ error_reporting(E_ALL);
 session_start();
 
 
-// --- SOLUCIÓN DE PERMISOS AUTOMÁTICA ---
-$ruta_imagenes = __DIR__ . '/imagenes/productos/';
-if (!file_exists($ruta_imagenes)) {
-    mkdir($ruta_imagenes, 0775, true);
-}
-// Forzamos los permisos en cada carga de la página para asegurar que PHP pueda escribir
-chmod($ruta_imagenes, 0775);
-
-
 require_once "controllers/controller.php";
 require_once "controllers/AuthController.php";
 require_once "controllers/CarritoController.php";
